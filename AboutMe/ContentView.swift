f9e9f490942e75267f78ff9997e5e8a9aa288479
triforcepotato <prieshajhala@gmail.com>
2023-07-13 13:49:26 -0700

    about me app!

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var fact1 = ""
+
     var body: some View {
+                
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+            
+            Text ("Priesha :)")
+                .font(.title)
+                .foregroundColor(Color.teal)
+                
+            HStack {
+                Image("Art")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                Image("choco")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+            } //hstack
+            
+            Button("Click here for fun facts!") {
+                
+                fact1 = ("I love dark chocolate")
+                
+            } //button
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.teal)
+            
+            
+            Text(fact1)
+            
+        } //vstack
+        
         .padding()
+        
+        
     }
 }
 
